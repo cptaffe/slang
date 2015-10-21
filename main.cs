040c6b3195afff0cd945e99addf0783fb4ef8fd6
Connor Taffe <cpaynetaffe@gmail.com>
2015-10-20 21:33:05 -0500

    infix expression parsing

--- main.cs
+++ main.cs
@@ -5,17 +5,42 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
 
-class Lexer : IEnumerable {
-  public struct Token {
-    public enum Type {
-      Integer,
-      FloatingPoint,
-      String,
-      Operator
-    };
-    public Type type;
-    public string str;
+struct Token {
+  public enum Type {
+    Integer,
+    FloatingPoint,
+    String,
+    Operator,
+    Newline
   };
+  public Type type;
+  public string str;
+
+  public struct Position {
+    public int i, line, col;
+
+    override public string ToString() {
+      return line+","+col;
+    }
+
+    public void Update(char c) {
+      i++;
+      if (c == '\n') {
+        col = 0;
+        line++;
+      } else {
+        col++;
+      }
+    }
+  }
+  public Position pos;
+
+  override public string ToString() {
+    return "("+pos+")->'"+str+"'";
+  }
+}
+
+class Lexer : IEnumerable<Token> {
   BlockingCollection<Token> _toks;
   IEnumerable<char> _src;
 
@@ -26,12 +51,19 @@ class Lexer : IEnumerable {
   }
 
   class Scanner {
-    Stack<char> _currentBuffer;
+
+    struct Character {
+      public char c;
+      public Token.Position pos;
+    }
+
+    Stack<Character> _currentBuffer;
     LinkedList<char> _toScan;
+    Token.Position pos;
 
     public Scanner(IEnumerable<char> str) {
       _toScan = new LinkedList<char>(str);
-      _currentBuffer = new Stack<char>();
+      _currentBuffer = new Stack<Character>();
     }
 
     public void AddInput(IEnumerable<char> str) {
@@ -40,14 +72,17 @@ class Lexer : IEnumerable {
 
     public char Next() {
       if (_toScan.Count == 0) return '\0';
-      _currentBuffer.Push(_toScan.First.Value);
+      var c = _toScan.First.Value;
+      _currentBuffer.Push(new Character(){ c = c, pos = pos });
+      pos.Update(c);
       _toScan.RemoveFirst();
-      Console.WriteLine("next: '"+_currentBuffer.Peek()+"'");
-      return _currentBuffer.Peek();
+      return c;
     }
 
     public void Back() {
-      _toScan.AddFirst(_currentBuffer.Pop());
+      var c = _currentBuffer.Pop();
+      pos = c.pos;
+      _toScan.AddFirst(c.c);
     }
 
     public char Peek() {
@@ -57,18 +92,17 @@ class Lexer : IEnumerable {
     }
 
     public void Clear() {
-      Console.WriteLine("'"+new String(_currentBuffer.ToArray())+"'");
       _currentBuffer.Clear();
     }
 
-    public IEnumerable<char> Get() {
-      return _currentBuffer.ToArray();
-    }
-
-    public string Emit() {
-      var s = new String(Get().ToArray());
+    public Token Emit(Token.Type type) {
+      string s = "";
+      foreach (var c in _currentBuffer) {
+        s += c.c;
+      }
+      Token.Position pos = _currentBuffer.ToArray()[0].pos;
       Clear();
-      return s;
+      return new Token(){ str = s, pos = pos, type = type };
     }
 
     public int Count() {
@@ -81,9 +115,12 @@ class Lexer : IEnumerable {
   StateFunc WhiteSpaceStateFactory(StateFunc n) {
     return delegate(Scanner s) {
       char c;
-      while ((c = s.Next()) == ' ' || c == '\t' || c == '\n');
+      while ((c = s.Next()) == ' ' || c == '\t' || c == '\n' || c == '\r') {
+        if (c == '\n') {
+          _toks.Add(s.Emit(Token.Type.Newline));
+        } else s.Clear(); // Whitespace is unimportant
+      }
       if (c != '\0') s.Back();
-      s.Clear(); // Whitespace is unimportant
       return n;
     };
   }
@@ -93,7 +130,7 @@ class Lexer : IEnumerable {
     while ((c = s.Next()) == '+' || c == '-' || c == '*' || c == '/') {}
     if (c != '\0') s.Back(); // Back off nonoperator
     if (s.Count() == 0) return null;
-    _toks.Add(new Token(){ str = s.Emit(), type = Token.Type.Operator });
+    _toks.Add(s.Emit(Token.Type.Operator));
     return WhiteSpaceStateFactory(NumberState);
   }
 
@@ -104,7 +141,7 @@ class Lexer : IEnumerable {
     if (s.Count() == 0) return null;
     if (c == '.') return null; // FloatingPoint number
     // Have lexed Integer.
-    _toks.Add(new Token(){ str = s.Emit(), type = Token.Type.Integer });
+    _toks.Add(s.Emit(Token.Type.Integer));
     return WhiteSpaceStateFactory(OperatorState);
   }
 
@@ -122,12 +159,104 @@ class Lexer : IEnumerable {
   IEnumerator IEnumerable.GetEnumerator() {
     return _toks.GetConsumingEnumerable().GetEnumerator();
   }
+
+  IEnumerator<Token> IEnumerable<Token>.GetEnumerator() {
+    return _toks.GetConsumingEnumerable().GetEnumerator();
+  }
+}
+
+class Parser {
+  IEnumerable<Token> _toks;
+  IEnumerator<Token> _toke;
+
+  public struct SyntaxTree {
+    public Token tok;
+    public List<SyntaxTree> children;
+
+    public SyntaxTree(Token t) {
+      tok = t;
+      children = new List<SyntaxTree>();
+    }
+
+    public void Add(SyntaxTree t) {
+      children.Add(t);
+    }
+
+    override public string ToString() {
+      string s = "";
+      s += tok;
+      if (children.Count > 0) {
+        s += "{";
+        foreach (var t in children) {
+          s += t+", ";
+        }
+        s += "}";
+      }
+      return s;
+    }
+  }
+
+  SyntaxTree _root;
+  BlockingCollection<SyntaxTree> _trees;
+
+  public Parser(IEnumerable<Token> toks) {
+    _toks = toks;
+    _trees = new BlockingCollection<SyntaxTree>();
+    new Thread(new ThreadStart(this.Run)).Start();
+  }
+
+  delegate StateFunc StateFunc();
+
+  int OperatorPrecedence(Token t) {
+    if (t.str == "+" || t.str == "-") {
+      return 2;
+    } else if (t.str == "*" || t.str == "/") {
+      return 1;
+    }
+    throw new Exception("Unknown Operator: "+t);
+  }
+
+  StateFunc ExpressionState() {
+    // Shunting Yard
+    var output = new Stack<SyntaxTree>();
+    var operators = new Stack<Token>();
+
+    foreach(var t in _toks) {
+      if (t.type == Token.Type.Integer) {
+        output.Push(new SyntaxTree(t));
+      } else if (t.type == Token.Type.Operator) {
+        while (operators.Count > 0 && OperatorPrecedence(t) > (OperatorPrecedence(operators.Peek()))) {
+          var st = new SyntaxTree(operators.Pop());
+          if (output.Count < 2) return null;
+          st.Add(output.Pop());
+          st.Add(output.Pop());
+          output.Push(st);
+        }
+        operators.Push(t);
+      }
+    }
+    while (operators.Count > 0) {
+      var st = new SyntaxTree(operators.Pop());
+      if (output.Count < 2) return null;
+      st.Add(output.Pop());
+      st.Add(output.Pop());
+      output.Push(st);
+    }
+    foreach (var t in output) {
+      Console.WriteLine(t);
+    }
+    return null;
+  }
+
+  public void Run() {
+    StateFunc func = ExpressionState;
+    while (func != null) func = func();
+    _trees.CompleteAdding();
+  }
 }
 
 class Lang {
   static void Main(string[] args) {
-    foreach (Lexer.Token t in new Lexer("3 + 4")) {
-      Console.WriteLine("tok: "+"'"+t.str+"'");
-    }
+    new Parser(new Lexer("3*5 + 4 * 8\n"));
   }
 }
